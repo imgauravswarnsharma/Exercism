08e3bb30e6727150130208ad25c02b9402029dd7
Gaurav Kumar <imgauravswarnsharma@gmail.com>
2023-02-12 01:06:54 +0530

    Code:Triangle Added

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,27 @@
-def equilateral(sides):
-    pass
+def valid_triangle(sides):
+    for i in range(3):
+        if sides[i]==0:
+            return False
+    if sides[0]+sides[1]>= sides[2]:
+        if sides[1]+sides[2]>= sides[0]:
+            if sides[2]+sides[0]>= sides[1]:
+                return True
+    return False
 
+def equilateral(sides):
+    if valid_triangle(sides) is True:
+        if sides[0]==sides[1]==sides[2]:
+            return True
+    return False
 
 def isosceles(sides):
-    pass
-
+    if valid_triangle(sides) is True:
+        if sides[0]==sides[1] or sides[1]==sides[2] or sides[0] ==sides[2]:
+            return True
+    return False
 
 def scalene(sides):
-    pass
+    if valid_triangle(sides) is True:
+        if sides[0]!=sides[1] and sides[1]!=sides[2] and sides[0] !=sides[2]:
+            return True
+    return False
