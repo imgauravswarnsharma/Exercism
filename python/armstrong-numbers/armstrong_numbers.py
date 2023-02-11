a0ff42a446250c1428b325e503e39bcc02f9bf14
Gaurav Kumar <imgauravswarnsharma@gmail.com>
2023-02-11 23:32:19 +0530

    Code Added: Armstrong no

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong_number(number):
-    pass
+    number = str(number)
+    n = len(number)
+    x = 0
+    for digit in range(n):
+        x += (int(number[digit]))**n
+    if x == int(number):
+        print(number)
+        return True
+    return False
