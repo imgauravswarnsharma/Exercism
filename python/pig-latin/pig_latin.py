6b094a38b49d9c5d82e62bccf00cdbda45c4bd83
Gaurav Kumar <imgauravswarnsharma@gmail.com>
2023-02-12 20:59:21 +0530

    Alternate Code Added

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -35,3 +35,27 @@ def translate(text):
             result_list.append(temp[n:]+temp[:n]+'ay')
 
     return " ".join(result_list )
+
+# Alternate Solution using sets and slices
+'''
+VOWELS = {"a", "e", "i", "o", "u"}
+VOWELS_Y = {"a", "e", "i", "o", "u", "y"}
+SPECIALS = {"xr", "yt"}
+
+
+def translate(text):
+    piggyfied = []
+
+    for word in text.split():
+        if word[0] in VOWELS or word[0:2] in SPECIALS:
+            piggyfied.append(word + "ay")
+            continue
+
+        for pos in range(1, len(word)):
+            if word[pos] in VOWELS_Y:
+                pos += 1 if word[pos] == 'u' and word[pos - 1] == "q" else 0
+                piggyfied.append(word[pos:] + word[:pos] + "ay")
+                break
+
+    return " ".join(piggyfied)
+'''
