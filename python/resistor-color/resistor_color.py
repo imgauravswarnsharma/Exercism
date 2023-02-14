6f22a4dc4212bea4c994748c7efe159cd27bf1b8
Gaurav Kumar <imgauravswarnsharma@gmail.com>
2023-02-15 00:58:30 +0530

    Code Added: Lists

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,7 @@
+def color_code(color):
+    return colors().index(color)
+
+
+def colors():
+    list_x = ["black", "brown", "red", "orange", "yellow","green","blue", "violet", "grey", "white"]
+    return list_x
